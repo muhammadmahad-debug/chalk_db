ae152fdbd803fc307153e06456027c9912e5b9e0
J.R Perez <hello@studiojerba.com>
2023-09-18 21:26:55 -0400

    Updated MongoDB

--- config/db.js
+++ config/db.js
@@ -1,23 +1,22 @@
-require('dotenv').config()
-const mongoose = require('mongoose')
+require("dotenv").config();
+const mongoose = require("mongoose");
 
 const connectDB = async () => {
   let a =
-    'mongodb+srv://hassanuet475:12345@stripe.7tkbit3.mongodb.net/stripe?retryWrites=true&w=majority'
+    "mongodb+srv://vercel-admin-user:P7GbE6JuVYIQJz6Y@cluster0.1x4m3es.mongodb.net/myFirstDatabase?retryWrites=true&w=majority";
   try {
     await mongoose.connect(a, {
       useFindAndModify: false,
       useNewUrlParser: true,
       useUnifiedTopology: true,
       useCreateIndex: true,
-    })
-    console.log('mongodb connection success!')
+    });
+    console.log("mongodb connection success!");
   } catch (err) {
-    console.log('mongodb connection failed!', err.message)
+    console.log("mongodb connection failed!", err.message);
   }
-}
+};
 
 module.exports = {
   connectDB,
-}
-
+};
