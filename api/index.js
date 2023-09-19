c176ade33c3dc4cdf07d30abb0a790be0f80401f
J.R Perez <hello@studiojerba.com>
2023-09-18 23:37:26 -0400

    Update

--- api/index.js
+++ api/index.js
@@ -1,29 +1,23 @@
-const express = require('express')
-const helmet = require('helmet')
-const cors = require('cors')
-const { connectDB } = require('../config/db')
-require('dotenv').config()
-const stripeRoutes= require('../routes/stripeRoute')
-const userRoutes = require('../routes/userRoute')
+const express = require("express");
+const helmet = require("helmet");
+const cors = require("cors");
+const { connectDB } = require("../config/db");
+require("dotenv").config();
+const stripeRoutes = require("../routes/stripeRoute");
+const userRoutes = require("../routes/userRoute");
 
+connectDB();
+const app = express();
 
-connectDB()
-const app = express()
+app.use(express.json({ limit: "4mb" }));
+app.use(helmet());
+app.use(cors());
 
-app.use(express.json({ limit: '4mb' }))
-app.use(helmet())
-app.use(cors())
+app.use("/api/stripe", stripeRoutes);
+app.use("/api/users", userRoutes);
 
-
-app.use('/api/stripe',stripeRoutes)
-app.use('/api/users', userRoutes)
-
-
-const PORT = process.env.PORT || 5000
+const PORT = process.env.PORT || 5000;
 
 app.listen(PORT, () => {
-  console.log(`Server is up and running on port ${PORT}`)
-})
-
-
-
+  console.log(`Server is up and running on port ${PORT}`);
+});
--- routes/stripeRoute.js
+++ routes/stripeRoute.js
@@ -1,15 +1,37 @@
-const express = require('express')
-const annualhandler = require('../controllers/annualSubscription')
-const monthlyhandler = require('../controllers/monthlySubscription')
-const webhookhandler = require('../controllers/webhook')
+// const express = require('express')
+// const annualhandler = require('../controllers/annualSubscription')
+// const monthlyhandler = require('../controllers/monthlySubscription')
+// const webhookhandler = require('../controllers/webhook')
 
+// const router = express.Router()
 
-const router = express.Router()
+// router.post('/annualsubscribe', annualhandler)
+// router.post('/monthlysubscribe', monthlyhandler)
+// router.post('/webhook', webhookhandler)
 
-router.post('/annualsubscribe', annualhandler)
-router.post('/monthlysubscribe', monthlyhandler)
-router.post('/webhook', webhookhandler)
+// module.exports = router
 
+const express = require("express");
+const cors = require("cors");
+const annualhandler = require("../controllers/annualSubscription");
+const monthlyhandler = require("../controllers/monthlySubscription");
+const webhookhandler = require("../controllers/webhook");
 
+const router = express.Router();
 
-module.exports = router
+// CORS options
+const corsOptions = {
+  origin: "https://join.chalkperformancetraining.com", // replace with your frontend application's URL
+  methods: "GET,HEAD,PUT,PATCH,POST,DELETE",
+  credentials: true,
+  optionsSuccessStatus: 204,
+};
+
+// Use CORS middleware
+router.use(cors(corsOptions));
+
+router.post("/annualsubscribe", annualhandler);
+router.post("/monthlysubscribe", monthlyhandler);
+router.post("/webhook", webhookhandler);
+
+module.exports = router;
