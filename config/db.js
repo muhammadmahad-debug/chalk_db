a03cef24d391111fb68df83fda78453f8c86c81f
J.R Perez <hello@studiojerba.com>
2023-09-19 00:23:02 -0400

    MongoDB

--- config/db.js
+++ config/db.js
@@ -2,7 +2,8 @@ require("dotenv").config();
 const mongoose = require("mongoose");
 
 const connectDB = async () => {
-  let a = process.env.MONGO_URI;
+  let a =
+    "mongodb+srv://vercel-admin-user:P7GbE6JuVYIQJz6Y@cluster0.1x4m3es.mongodb.net/myFirstDatabase?retryWrites=true&w=majority";
   try {
     await mongoose.connect(a, {
       useFindAndModify: false,
