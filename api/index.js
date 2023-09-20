404b112a5fb8032f235f9d655a3e3cfbc35f4296
muhammmadmahad-debug <mahadhashmi2@gmail.com>
2023-09-20 14:51:47 +0500

    fix-cors error

--- api/index.js
+++ api/index.js
@@ -5,13 +5,13 @@ const { connectDB } = require("../config/db");
 require("dotenv").config();
 const stripeRoutes = require("../routes/stripeRoute");
 const userRoutes = require("../routes/userRoute");
-
-connectDB();
 const app = express();
 app.use(cors());
 
+
 app.use(express.json({ limit: "4mb" }));
 app.use(helmet());
+connectDB();
 
 app.use("/api/stripe", stripeRoutes);
 app.use("/api/users", userRoutes);
